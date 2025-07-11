0ac66df070d036d93907f7d32f699128fe4c95fe
Paul Gerasymchuk <pgerasymchuk@gmail.com>
2025-07-12 00:28:19 +0300

    Added Automapper profile for Url

---
+++ WebApi/MappingProfiles/UrlProfile.cs
@@ -0,0 +1,18 @@
+﻿using Application.URLs.Dtos;
+using AutoMapper;
+using Domain.Entities;
+
+namespace WebAPI.MappingProfiles;
+
+public class UrlProfile : Profile
+{
+    public UrlProfile()
+    {
+        CreateMap<Url, UrlDetailedDto>()
+            .ForMember(dest => dest.ShortUrl, opt => opt.MapFrom(src => $"/r/{src.ShortCode}"));
+        
+        CreateMap<Url, UrlBasicDto>()
+            .ForMember(dest => dest.ShortUrl, opt => opt.MapFrom(src => $"/r/{src.ShortCode}"));
+
+    }
+}
