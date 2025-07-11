eec101a66e14a60f16b5a0e7c3164c2c42fa4a76
Paul Gerasymchuk <pgerasymchuk@gmail.com>
2025-07-12 00:15:11 +0300

    Added URLs dtos

---
+++ Application/URLs/Dtos/UrlBasicDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.URLs.Dtos;
+
+public class UrlBasicDto
+{
+    public Guid Id { get; set; }
+    
+    public string OriginalUrl { get; set; }
+    
+    public string ShortUrl { get; set; }
+}
---
+++ Application/URLs/Dtos/UrlDetailedDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.URLs.Dtos;
+
+public class UrlDetailedDto
+{
+    public Guid Id { get; set; }
+    
+    public string OriginalUrl { get; set; }
+    
+    public string ShortUrl { get; set; }
+    
+    public DateTime CreatedAt { get; set; }
+}
