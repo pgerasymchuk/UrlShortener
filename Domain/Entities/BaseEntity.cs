6ba1ec5b3a348559bdaa99dddccac6b01a6d7ff2
Paul Gerasymchuk <pgerasymchuk@gmail.com>
2025-07-12 00:12:04 +0300

    Added entities

---
+++ Domain/Entities/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entities;
+
+public abstract class BaseEntity<TKey>
+{
+    public TKey Id { get; set; }
+    
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public DateTime? DeletedAt { get; set; }
+}
---
+++ Domain/Entities/Url.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entities;
+
+public class Url : BaseEntity<Guid>
+{
+    public string OriginalUrl { get; set; }
+    
+    public string ShortCode { get; set; }
+    
+    public Guid? UserId { get; set; }
+}
