b3c66ae2ab99093aa5629d774dfed9c45c07b34b
Paul Gerasymchuk <pgerasymchuk@gmail.com>
2025-07-12 00:12:21 +0300

    Added repository interfaces

---
+++ Domain/Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+
+namespace Domain.Interfaces.Repositories;
+
+public interface IRepository<TEntity, in TKey>
+{
+    Task<IEnumerable<TEntity>> GetAllAsync();
+    
+    Task<TEntity?> GetByIdAsync(TKey id);
+    
+    Task AddAsync(TEntity entity);
+    
+    Task DeleteByIdAsync(TKey id);
+}
---
+++ Domain/Interfaces/Repositories/IUrlRepository.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+
+namespace Domain.Interfaces.Repositories;
+
+public interface IUrlRepository : IRepository<Url, Guid>
+{
+    Task<bool> DoesShortCodeExistAsync(string shortCode);
+    
+    Task<IEnumerable<Url>> GetAllByUserIdAsync(Guid userId);
+    
+    Task<string?> GetOriginalUrlByShortCodeAsync(string shortCode);
+}
