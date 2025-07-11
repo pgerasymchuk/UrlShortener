ccaedec53a9120d843c1bf7ee47895074ee46c5b
Paul Gerasymchuk <pgerasymchuk@gmail.com>
2025-07-12 00:12:46 +0300

    Added AppDbContext

---
+++ Infrastructure/Persistence/Context/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Application.Identity;
+using Domain.Entities;
+using Infrastructure.Persistence.Extensions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence.Context;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options)
+    : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>(options)
+{
+    public DbSet<Url> ShortenedUrls { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<Url>()
+            .HasIndex(x => x.ShortCode)
+            .IsUnique();
+
+        builder.Entity<Url>()
+            .HasQueryFilter(x => x.DeletedAt == null);
+        
+        builder.SeedUsers();
+    }
+}
